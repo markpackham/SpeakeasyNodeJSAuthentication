cacc85df3d80f25ac7617f312afbd2fb3562ff95
mark <markpackham1@gmail.com>
2020-11-13 14:16:09 +0000

    finish off with validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Speakeasy 2 Factor Authentication learned from https://www.youtube.com/watch?v=KQya9i6czhM
 const express = require("express");
 const bodyParser = require("body-parser");
 // could use any database, this local json one is easy to use for testing
@@ -34,10 +35,16 @@ http: app.post("/api/register", (req, res) => {
 });
 
 // Verify token and make secret permnant
-// Need to use Insominia or Postman to check and use JSON
+// Need to use Insominia or Postman to check and use JSON, get info from database after registering
+// REMEMBER Tokens valid only for a LIMITED period of time on Authenticator
+// To get the token you need to get the Base32 eg EUXW4MCBMZ4F2VCNGNNCM3ZFHBKCKQ3DGRTWWZRPHNPEC3TPMJAA
+// and run that through an authenticator app
 // localhost:5000/api/verify
 /*
-
+{
+    "userId": "273f0343-9055-4acb-884a-d1d24ea74a8f"
+    "token": "895481"
+}
 */
 app.post("/api/verify", (req, res) => {
   const { token, userId } = req.body;
@@ -59,6 +66,42 @@ app.post("/api/verify", (req, res) => {
     } else {
       // in a real app you'd get the front end to yell at the user when they are verified as false
       res.json({ verified: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
+  }
+});
+
+// Validate token (like verify but more constant, not a one off login)
+//localhost:5000/api/register
+// use Insomnia to test, have JSON simular to verify
+// REMEMBER Tokens valid only for a LIMITED period of time on Authenticator
+/*
+{
+    "userId": "273f0343-9055-4acb-884a-d1d24ea74a8f"
+    "token": "895481"
+}
+*/
+app.post("/api/validate", (req, res) => {
+  const { userId, token } = req.body;
+  try {
+    // Retrieve user from database
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    console.log({ user });
+    const { base32: secret } = user.secret;
+    // Returns true if the token matches
+    const tokenValidates = speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token,
+      window: 1,
+    });
+    if (tokenValidates) {
+      res.json({ validated: true });
+    } else {
+      res.json({ validated: false });
     }
   } catch (error) {
     console.error(error);
