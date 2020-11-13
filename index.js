5bdbb76bd1df1c077ffdae47953b3b2ca9578dce
mark <markpackham1@gmail.com>
2020-11-13 13:22:35 +0000

    verify token, https://youtu.be/KQya9i6czhM?t=980

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const uuid = require("uuid");
 const speakeasy = require("speakeasy");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const db = new JsonDB(new Config("myDatabase", true, false, "/"));
+
+app.use(express.json());
 
 app.get("/api", (req, res) =>
   res.json({ message: "Welcome to the 2 factor auth example" })
@@ -22,11 +23,27 @@ app.post("/api/register", (req, res) => {
     const path = `/user/${id}`;
     const temp_secret = speakeasy.generateSecret();
     db.push(path, { i, temp_secret });
-    res.json({ id, secret: temp_secret });
+    res.json({ id, secret: temp_secret.base32 });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error generating secret" });
   }
+});
+
+// Verify token and make secret permnant
+app.post("/api/verify", (req, res) => {
+  const { token, userId } = req.body;
+
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    const { base32: secret } = user.temp_secret;
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token,
+    });
+  } catch (error) {}
 });
 
 app.listen(PORT, () => {
